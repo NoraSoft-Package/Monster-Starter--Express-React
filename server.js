70e8389e9b4a064c1477a71969a7f85dd0e20a57
Monster-Mohamed <ma8494927@gmail.com>
2022-06-19 08:10:11 +0200

    MSER v2.1 Modified server file

--- server.js
+++ server.js
@@ -2,10 +2,26 @@ const express = require("express");
 
 const app = express();
 
+// body parser
+const bodyParser = require("body-parser");
+
+// parse application/x-www-form-urlencoded
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+// parse application/json
+app.use(bodyParser.json());
+
+// api routes
 app.get("/api/customers", (req, res) => {
   res.send("Customers are coming...");
 });
 
+// server port
 const port = 5000;
 
+// to run the server
 app.listen(port, () => `Server running on port ${port}`);
